f146290aced726b6b15a7bf8cbd4fb26a3caf797
stefan paicu <stefan@triplemars.com>
2022-01-26 23:37:36 +0200

    [Small] Commented Controller

--- OrderService/Controllers/OrderController.cs
+++ OrderService/Controllers/OrderController.cs
@@ -12,54 +12,54 @@ namespace OrderService.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-        private readonly IOrderManager orderManager;
+        //private readonly IOrderManager orderManager;
 
-        public OrderController(IOrderManager orderManager)
-        {
-            this.orderManager = orderManager;
-        }
+        //public OrderController(IOrderManager orderManager)
+        //{
+        //    this.orderManager = orderManager;
+        //}
 
-        [HttpPost("create")]
-        public async Task<IActionResult> CreateAsync([FromBody] Order order)
-        {
-            await orderManager.CreateOrder(order);
-            return NoContent();
-        }
+        //[HttpPost("create")]
+        //public async Task<IActionResult> CreateAsync([FromBody] Order order)
+        //{
+        //    await orderManager.CreateOrder(order);
+        //    return NoContent();
+        //}
 
-        [HttpPut("update")]
-        public async Task<IActionResult> UpdateAsync([FromBody] Order order)
-        {
-            await orderManager.UpdateOrder(order);
-            return NoContent();
-        }
+        //[HttpPut("update")]
+        //public async Task<IActionResult> UpdateAsync([FromBody] Order order)
+        //{
+        //    await orderManager.UpdateOrder(order);
+        //    return NoContent();
+        //}
 
-        [HttpDelete("delete/{id}")]
-        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
-        {
-            await orderManager.DeleteOrder(id);
-            return NoContent();
-        }
+        //[HttpDelete("delete/{id}")]
+        //public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        //{
+        //    await orderManager.DeleteOrder(id);
+        //    return NoContent();
+        //}
 
-        [HttpPut("cancel/{id}")]
-        public async Task<IActionResult> CancelAsync([FromRoute] int id)
-        {
-            var response = await orderManager.CancelOrder(id);
-            return response == true ? Ok() : BadRequest();
-        }
+        //[HttpPut("cancel/{id}")]
+        //public async Task<IActionResult> CancelAsync([FromRoute] int id)
+        //{
+        //    var response = await orderManager.CancelOrder(id);
+        //    return response == true ? Ok() : BadRequest();
+        //}
 
-        [HttpPost("success/{id}")]
-        public async Task<IActionResult> SuccessAsync([FromRoute] int id)
-        {
-            var response = await orderManager.SuccessOrder(id);
-            return Ok();
-        }
+        //[HttpPost("success/{id}")]
+        //public async Task<IActionResult> SuccessAsync([FromRoute] int id)
+        //{
+        //    var response = await orderManager.SuccessOrder(id);
+        //    return Ok();
+        //}
 
-        [HttpPost("fail/{id}")]
-        [Authorize("Manager")]
-        public async Task<IActionResult> FailAsync([FromRoute] int id)
-        {
-            var response = await orderManager.FailOrder(id);
-            return response == true ? Ok() : BadRequest();
-        }
+        //[HttpPost("fail/{id}")]
+        //[Authorize("Manager")]
+        //public async Task<IActionResult> FailAsync([FromRoute] int id)
+        //{
+        //    var response = await orderManager.FailOrder(id);
+        //    return response == true ? Ok() : BadRequest();
+        //}
     }
 }
